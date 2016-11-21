9a75dd93ca8b51448f960550b5a883d39eca5ee4
br0r <viktor@silfverstrom.com>
2016-11-21 17:36:08 +0100

    removed cors and updated microstar-track

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ const config = getConfig(process.env.NODE_ENV);
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use((req, res, next) => {
   //Sets json to pretty when asking for it
   //Check if we need to do this on each request.
